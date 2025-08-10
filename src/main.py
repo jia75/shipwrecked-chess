a35f4788ee8559b316c319b774cb2bdeee6c2eb7
Carter Pfaff <carterpfaff@gmail.com>
2025-08-10 12:14:15 -0400

    Add names for pieces in display

--- src/main.py
+++ src/main.py
@@ -43,14 +43,24 @@ class App(badge.BaseApp):
         badge.display.rect(x, y, 15, 15, 0)
         if piece == 0:
             return
+        piece_mappings = {
+            1: "P",
+            2: "N",
+            3: "B",
+            4: "R",
+            5: "Q",
+            6: "K",
+        }
+        ptype = piece % 10
+        letter = piece_mappings.get(ptype, "?")
         if piece//10 == 1:
-            badge.display.nice_text(str(piece%10), x, y, 18, rot=0)
+            badge.display.nice_text(letter, x, y, 18, rot=0)
         elif piece//10 == 2:
-            badge.display.nice_text(str(piece%10), x+14, y, 18, rot=90)
+            badge.display.nice_text(letter, x+14, y, 18, rot=90)
         elif piece//10 == 3:
-            badge.display.nice_text(str(piece%10), x+14, y+14, 18, rot=180)
+            badge.display.nice_text(letter, x+14, y+14, 18, rot=180)
         elif piece//10 == 4:
-            badge.display.nice_text(str(piece%10), x, y+14, 18, rot=270)
+            badge.display.nice_text(letter, x, y+14, 18, rot=270)
 
     def move_board_to_buffer(self, board: List[List[int]], player_number: int) -> None:
         for column_index in range(14):
