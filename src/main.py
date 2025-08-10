2f06da474a9394a913a1160d7239a61bc93eb524
Carter Pfaff <carterpfaff@gmail.com>
2025-08-10 02:21:11 -0400

    Add comms for movement

--- src/main.py
+++ src/main.py
@@ -16,22 +16,32 @@ class App(badge.BaseApp):
         self.state = "Lobby"
 
     def join_lobby(self, hostId) -> None:
+        # 1321
         if self.isHost:
             raise RuntimeError("Cannot join lobby as host")
         badge.radio.send_packet(hostId, "join_request".encode('utf-8'))
         self.state = "Lobby"
         
+    def handle_move(self, player, move):
+        pass
+    
+    def send_move(self, move):
+        if self.state != "Game":
+            raise RuntimeError("Cannot send move, not in game state")
+        for player in self.players:
+            badge.radio.send_packet(player, f"move:{move}".encode('utf-8'))
+
     def on_packet(self, packet, is_foreground):
         if (self.isHost and packet.data == "join_request".encode('utf-8')):
             new_player = packet.source
             if new_player not in self.players:
                 self.players.append(new_player)
-                badge.radio.send_packet(new_player, "join_accepted".encode('utf-8'))
+                badge.radio.send_packet(new_player, f"join_accepted:{','.join(self.players)}".encode('utf-8'))
                 badge.display.show_text(f"Player {new_player} joined")
                 for player in self.players.remove(new_player):
                     badge.radio.send_packet(player, f"player_joined:{new_player}".encode('utf-8'))
             else:
-                badge.display.show_text(f"Player {new_player} already in lobby")
+                pass
         elif (not self.isHost and packet.data == "join_accepted".encode('utf-8')):
             badge.display.show_text("Joined lobby")
             self.players.append(packet.source)
@@ -40,4 +50,9 @@ class App(badge.BaseApp):
             new_player = packet.data.decode('utf-8').split(":")[1]
             if new_player not in self.players:
                 self.players.append(new_player)
-                badge.display.show_text(f"New player joined: {new_player}")
+        elif packet.data.startswith(b"join_accepted:"):
+            players_list = packet.data.decode('utf-8').split(":")[1]
+            self.players = players_list.split(",")
+        elif packet.data.startswith(b"move:"):
+            move = packet.data.decode('utf-8').split(":")[1]
+            self.handle_move(packet.source, move)
